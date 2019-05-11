c0eca206d81c494c6be55c86d15eb24d9458aa11
brittrademaker <rademaker.britt@hotmail.com>
2019-05-11 13:28:16 +0200

    Base of artwork

--- closed-captions/yt.js
+++ closed-captions/yt.js
@@ -2,8 +2,8 @@ var player;
 
 function onYouTubeIframeAPIReady() {
 	player = new YT.Player('video-placeholder', {
-		width: 600,
-		height: 400,
+//		width: 600,
+//		height: 400,
 		videoId: 'vrP-_T-h9YM',
 		playerVars: {
 			color: 'white'
